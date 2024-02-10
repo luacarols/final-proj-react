ffe2adff23804badf272defbd420c62f4361e48a
Luana Belnuovo <luanacbsoares@gmail.com>
2024-02-10 00:36:47 -0300

    Ajuste de espaço no App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
 
   return (
     <BrowserRouter>
-      <Menu/>
+      <Menu />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sobre" element={<Sobre />} />
